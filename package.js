ce192e67dcb27c2a3de4f9f7013b2b2dd9a757f0
Amol Rajendra Dhawale <103090142+Amold2002@users.noreply.github.com>
2022-06-07 20:43:18 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,30 @@
+{
+   "name": "all-about-package-json-file",
+   "version": "1.0.0",
+   "description": "a node js tutorial",
+   "main": "index.js",
+   "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1"
+   },
+   "repository": {
+      "type": "git",
+      "url": "git+https://github.com/Amold2002/node.js.git"
+   },
+   "keywords": [
+      "code",
+      "step",
+      "by",
+      "step"
+   ],
+   "author": "Amol Dhawale",
+   "license": "ISC",
+   "bugs": {
+      "url": "https://github.com/Amold2002/node.js/issues"
+   },
+   "homepage": "https://github.com/Amold2002/node.js#readme",
+   "dependencies": {
+      "chalk": "^5.0.1",
+      "console-log-colors": "^0.2.3",
+      "simple-node-logger": "^21.8.12"
+   }
+}
